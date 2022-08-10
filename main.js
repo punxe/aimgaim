206c010e799ae3a154259e571a6ba0339652e877
punxe <dsammsh@gmail.com>
2022-08-09 17:48:48 -0700

    Added basic UI

--- main.js
+++ main.js
@@ -2,15 +2,37 @@ var canvas, context;
 var targets = [];
 var targetRadius = 50;
 var targetColor = 'black';
+var canvasLeft, canvasTop;
 
 
 // wait for DOM to load
 document.addEventListener('DOMContentLoaded', init);
 
 // initialization
-function init (){
+function init() {
+   
     canvas = document.getElementById("gameCanvas");
     context = canvas.getContext("2d");
+
+
+
+    canvas.width = window.screen.width * 0.7;
+    canvas.height = window.screen.height * 0.7;
+    targetRadius = window.screen.height * 0.07;
+
+    canvasLeft = canvas.offsetLeft;
+    canvasTop = canvas.offsetTop;
+
+    // add event listener to detect mouse click
+    canvas.addEventListener('mousedown', (event) => {
+        console.log("click: x=" + event.x + " y=" + event.y);
+        for (var i = 0; i < targets.length; i++) {
+            if (checkTargetHit(event.x - canvasLeft, event.y - canvasTop, targets[i])) {
+                targets[i].delete();
+                generateNewTarget();
+            }
+        }
+    })
 
     generateNewTarget();
     generateNewTarget();
@@ -18,19 +40,19 @@ function init (){
 }
 
 // create a new target
-function generateNewTarget(){
+function generateNewTarget() {
     var x, y;
-    do{
-        var x = 200+Math.floor(Math.random()*500);
-        var y = 200+Math.floor(Math.random()*500);
-    }while(checkTargetOverlap(x, y));
+    do {
+        var x = targetRadius + Math.floor(Math.random() * (canvas.width - 2 * targetRadius));
+        var y = targetRadius + Math.floor(Math.random() * (canvas.height - 2 * targetRadius));
+    } while (checkTargetOverlap(x, y));
     targets.push(new Target(x, y));
 }
 
 // check if a specified coordinates for a potential target would overlap with any existing targets
-function checkTargetOverlap(x, y){
-    for(var i = 0; i < targets.length; i++){
-        if(Math.abs(x - targets[i].x) < 2 * targetRadius && Math.abs(y - targets[i].y) < 2 * targetRadius && !targets[i].deleted){
+function checkTargetOverlap(x, y) {
+    for (var i = 0; i < targets.length; i++) {
+        if (Math.abs(x - targets[i].x) < 2 * targetRadius && Math.abs(y - targets[i].y) < 2 * targetRadius && !targets[i].deleted) {
             return true;
         }
     }
@@ -38,12 +60,12 @@ function checkTargetOverlap(x, y){
 }
 
 // check if the user clicked on the specified target
-function checkTargetHit(x, y, target){
+function checkTargetHit(x, y, target) {
     return Math.abs(x - target.x) < targetRadius && Math.abs(y - target.y) < targetRadius && !target.deleted;
 }
 
 // draw a target on the canvas at the specified x and y coordinates
-function drawTarget(x, y){
+function drawTarget(x, y) {
     context.beginPath();
     context.strokeStyle = targetColor;
     context.fillStyle = targetColor;
@@ -53,35 +75,26 @@ function drawTarget(x, y){
 }
 
 // delete the target at the specified x and y coordinates
-function deleteTarget(x, y){
+function deleteTarget(x, y) {
     context.beginPath();
     context.strokeStyle = 'white';
     context.fillStyle = 'white';
-    context.arc(x, y, targetRadius+1, 0, 2 * Math.PI);
+    context.arc(x, y, targetRadius + 1, 0, 2 * Math.PI);
     context.fill();
     context.stroke();
 }
-    
+
 // Target class to store coordinates of a target as well as a function to delete the target
 class Target {
-    constructor(x, y){
+    constructor(x, y) {
         this.x = x;
         this.y = y;
         this.deleted = false;
         drawTarget(x, y);
     }
-    delete(){
+    delete() {
         deleteTarget(this.x, this.y);
         this.deleted = true;
     }
 }
 
-// add event listener to detect mouse click
-document.addEventListener('click', (event) => {
-    for(var i = 0; i < targets.length; i++){
-        if(checkTargetHit(event.x, event.y, targets[i])){
-            targets[i].delete();
-            generateNewTarget();
-        }
-    }
-  })
